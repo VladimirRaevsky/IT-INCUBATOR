e4a56dc534e26fcf6a3c3e0d9359260ed4877c6a
Vladimir Raevskiy <rayevskiy.volodya@mail.ru>
2023-01-24 20:39:48 +0300

    task with CODEWARS JS number fibonacci

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 
-/*
-  task with FREECODECAMP JS
-*/
+/*******************************task with FREECODECAMP JS *******************************/
 //one
 // This is an in-line comment.
 /* This is a
@@ -18,9 +16,8 @@ var a;
 var a = 7;
 
 
-/*
-  task with FREECODECAMP HTML
-*/
+/*******************************task with FREECODECAMP HTML*****************************/
+
 //one
 <h1>Hello World</h1>
   
@@ -38,7 +35,18 @@ var a = 7;
 {<h2>CatPhotoApp</h2>}
 {<p>Kitty Ipsum</p>}
 
-//
+//five
 <h1>Hello World</h1>
 {<h2>CatPhotoApp</h2>}
 <p>Kitty ipsum dolor sit amet, shed everywhere shed everywhere stretching attack your ankles chase the red dot, hairball run catnip eat the grass sniff.</p>
+
+
+/*******************************task with CODEWARS JS ***********************************/
+//fibonacci
+function nthFibo(n) {
+  let [prev, curr] = [0, 1];
+  for (let i = 1; i < n; i++) [prev, curr] = [curr, prev + curr];
+  return prev;
+}
+
+nthFibo(10)
